259fa19e57987b178def5b9e71db04f86569d9ef
asim-tx <asim@techcreatix.com>
2019-07-31 17:11:58 +0500

    ahsan9

--- db/migrate/20190724112152_change_type_interview.rb
+++ db/migrate/20190724112152_change_type_interview.rb
@@ -1,6 +1,6 @@
 class ChangeTypeInterview < ActiveRecord::Migration[5.2]
   def change
     change_column :interviews, :time, 'time USING CAST(time AS time)'
-    change_column :interviews, :date, :date
+    change_column :interviews, :date, 'date USING CAST(date AS date)'
   end
 end
