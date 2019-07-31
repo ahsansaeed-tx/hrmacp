91cdc93065ee4b46404e8ffecd0880c631acb96f
asim-tx <asim@techcreatix.com>
2019-07-31 16:46:58 +0500

    ahsan4

--- db/migrate/20190724112152_change_type_interview.rb
+++ db/migrate/20190724112152_change_type_interview.rb
@@ -1,6 +1,6 @@
 class ChangeTypeInterview < ActiveRecord::Migration[5.2]
   def change
-    change_column :interviews, :time, :time
+    change_column :interviews, :time, :datetime
     change_column :interviews, :date, :date
   end
 end
