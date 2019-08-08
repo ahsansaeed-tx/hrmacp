1f0f9c0f379b86d1f679e78fe65682fc12eb93bf
asim-tx <asim@techcreatix.com>
2019-08-09 00:28:51 +0500

    thursday committt

--- db/migrate/20190806172214_add_default_tosalary_column.rb
+++ db/migrate/20190806172214_add_default_tosalary_column.rb
@@ -1,5 +1,5 @@
 class AddDefaultTosalaryColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column_default :users, :salary, 00000
+    change_column_default :users, :salary, 0
   end
 end
