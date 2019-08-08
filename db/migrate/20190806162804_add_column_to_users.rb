677fb95b28e34e0899ac608b19dc2c8d5e4eb2ae
asim-tx <asim@techcreatix.com>
2019-08-09 00:21:56 +0500

    thursday committ

--- db/migrate/20190806162804_add_column_to_users.rb
+++ db/migrate/20190806162804_add_column_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    # add_column :users, :salary, :number
     add_column :users, :salary, :integer
+    # add_column :users, :salary, :number
   end
 end
