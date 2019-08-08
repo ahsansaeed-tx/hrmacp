9839f962b14fb9b84ffc858fe6637392ffa5072f
asim-tx <asim@techcreatix.com>
2019-08-09 00:13:03 +0500

    thursday commit

--- db/migrate/20190806162804_add_column_to_users.rb
+++ db/migrate/20190806162804_add_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :salary, :number
+    # add_column :users, :salary, :number
   end
 end
--- db/migrate/20190807135416_change_salary_type.rb
+++ db/migrate/20190807135416_change_salary_type.rb
@@ -1,5 +1,6 @@
 class ChangeSalaryType < ActiveRecord::Migration[5.2]
   def change
-    change_column :users, :salary, :float
+    # change_column :users, :salary, :float
+    change_column :users, :salary, 'float USING CAST(salary AS float)'
   end
 end
