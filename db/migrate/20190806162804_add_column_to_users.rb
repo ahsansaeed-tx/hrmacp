c47f25af63818dfaa25377f548b062849d033989
asim-tx <asim@techcreatix.com>
2019-08-19 16:03:49 +0500

    monday commit

--- db/migrate/20190806162804_add_column_to_users.rb
+++ db/migrate/20190806162804_add_column_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :salary, :float, default: 0.0
+    # add_column :users, :salary, :float, default: 0.0
     # add_column :users, :salary, :number
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_174114) do
+ActiveRecord::Schema.define(version: 2019_08_09_114811) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
